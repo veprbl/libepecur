c5e3b9afbc3b81b2ecb35699f334b8990e521c3a
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2012-01-22 23:42:13 +0400

    moved MomentumDispersionApp to main.hpp

--- src/utils/momentum_dispersion/main.cpp
+++ src/utils/momentum_dispersion/main.cpp
@@ -14,32 +14,12 @@
 #include <epecur/geometry.hpp>
 #include <epecur/loadfile.hpp>
 
+#include "main.hpp"
 #include "EvSumHook.hpp"
 
 namespace po = boost::program_options;
 
 using namespace std;
-
-class	MomentumDistributionApp: public TApplication
-{
-private:
-	TCanvas*	main_canvas;
-	vector<TH1I*>	th;
-
-	void	ParseCommandLine( int argc, char* argv[] );
-
-public:
-	MomentumDistributionApp(
-		const char* appClassName, Int_t* argc, char** argv, void* options = 0, Int_t numOptions = 0
-		);
-	~MomentumDistributionApp();
-
-	string	data_filepath;
-	string	geometry_filepath;
-
-	void	Init();
-	void	PlotResults( uint planes[][200] );
-};
 
 MomentumDistributionApp::MomentumDistributionApp(
 	const char* appClassName, Int_t* argc, char** argv, void* options, Int_t numOptions
---
+++ src/utils/momentum_dispersion/main.hpp
@@ -0,0 +1,25 @@
+#ifndef __MAIN_HPP
+#define __MAIN_HPP
+
+class	MomentumDistributionApp: public TApplication
+{
+private:
+	TCanvas*	main_canvas;
+	vector<TH1I*>	th;
+
+	void	ParseCommandLine( int argc, char* argv[] );
+
+public:
+	MomentumDistributionApp(
+		const char* appClassName, Int_t* argc, char** argv, void* options = 0, Int_t numOptions = 0
+		);
+	~MomentumDistributionApp();
+
+	string	data_filepath;
+	string	geometry_filepath;
+
+	void	Init();
+	void	PlotResults( uint planes[][200] );
+};
+
+#endif
