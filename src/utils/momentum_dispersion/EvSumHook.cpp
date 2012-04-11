ab6ccb4c47b682c5d09cc23c5b9151dacc5c2afb
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2012-04-11 21:27:53 +0400

    fixed rounding bug in momentum_dispersion

--- src/utils/momentum_dispersion/EvSumHook.cpp
+++ src/utils/momentum_dispersion/EvSumHook.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include <cmath>
 
 #include <epecur/types.hpp>
 
@@ -16,7 +17,13 @@ void	EvSumHook::handle_prop_data( const wire_id_t* begin, const wire_id_t* end, 
 	for(auto pos = begin; pos < end; pos++)
 	{
 		wire_pos_t	wire_pos = geom.get_wire_pos(dev_id, *pos);
+		int	bin = int(floor(wire_pos)) + WIRES_COUNT / 2;
 
-		chambers[chamber_id][int(wire_pos) + 100] += 1;
+		if ((bin < 0) || (bin >= WIRES_COUNT))
+		{
+			continue;
+		}
+
+		chambers[chamber_id][bin] += 1;
 	}
 }
