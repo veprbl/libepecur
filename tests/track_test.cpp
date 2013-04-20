31fafd849c9b60989cf719c30c3cd8d799b3a78c
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2013-04-20 17:22:06 +0400

    test drift track recognition

--- tests/track_test.cpp
+++ tests/track_test.cpp
@@ -102,4 +102,34 @@ BOOST_AUTO_TEST_CASE( check_recognize_all_tracks_prop )
 	BOOST_CHECK_EQUAL(tracks[1].c1, 1);
 }
 
+BOOST_AUTO_TEST_CASE( check_recognize_all_tracks_drift )
+{
+	vector<wire_pos_t>	v1;
+	v1.push_back(1); v1.push_back(2);
+	vector<wire_pos_t>	v2;
+	v2.push_back(1); v2.push_back(2);
+	vector<wire_pos_t>	v3;
+	v3.push_back(2); v3.push_back(3);
+	vector<wire_pos_t>	v4;
+	v4.push_back(2); v4.push_back(3);
+
+	vector< vector<wire_pos_t>* >	data;
+	data.push_back(&v1);
+	data.push_back(&v2);
+	data.push_back(&v3);
+	data.push_back(&v4);
+
+	vector<double>	normal_pos;
+	normal_pos.push_back(1);
+	normal_pos.push_back(2);
+	normal_pos.push_back(3);
+	normal_pos.push_back(4);
+
+	vector<track_info_t>	tracks = recognize_all_tracks<track_type_t::drift>(data, normal_pos);
+
+	BOOST_REQUIRE_EQUAL(tracks.size(), 1u);
+	BOOST_CHECK_EQUAL(tracks[0].c0, 2);
+	BOOST_CHECK_EQUAL(tracks[0].c1, 0);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
