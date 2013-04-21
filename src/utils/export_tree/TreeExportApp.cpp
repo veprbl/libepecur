70f4879c93e9e87d7a9251f99fea36d04ea66095
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2013-04-21 13:17:31 +0400

    export_tree: --rough-dirft option

--- src/utils/export_tree/TreeExportApp.cpp
+++ src/utils/export_tree/TreeExportApp.cpp
@@ -25,6 +25,7 @@ using namespace std;
 string data_filepath;
 string geometry_filepath;
 string output_filepath;
+bool rough_drift;
 
 void	ParseCommandLine( int argc, char* argv[] )
 {
@@ -37,6 +38,7 @@ void	ParseCommandLine( int argc, char* argv[] )
 		("help,h", "produce this output")
 		("geometry-file,g", po::value<string>(), "specify geometry description file")
 		("output-file,o", po::value<string>(), "specify output file")
+		("rough-drift", "disable using time information for drift tracks")
 		;
 	cmdline_options.add(visible);
 
@@ -68,6 +70,7 @@ void	ParseCommandLine( int argc, char* argv[] )
 	data_filepath = vm["input-file"].as< vector<string> >()[0];
 	geometry_filepath = vm["geometry-file"].as<string>();
 	output_filepath = vm["output-file"].as<string>();
+	rough_drift = vm.count("rough-drift");
 }
 
 void	fill_info( TTree &info )
@@ -105,20 +108,26 @@ int	main( int argc, char* argv[] )
 	Geometry	geom(file);
 	TFile		tree_file(output_filepath.c_str(), "RECREATE");
 	TTree		info("info", "information about this file");
-	DriftCalibHook	calib_hook(geom);
+	StdDrift::calibration_curve_t	*calibration_curve = NULL;
 
-	try
+	if (!rough_drift)
 	{
-		loadfile(data_filepath, calib_hook);
-	}
-	catch( const char *err )
-	{
-		cerr << err << endl;
+		DriftCalibHook	calib_hook(geom);
+
+		try
+		{
+			loadfile(data_filepath, calib_hook);
+		}
+		catch( const char *err )
+		{
+			cerr << err << endl;
+		}
+
+		calib_hook.generate_calibration_curves();
+		calibration_curve = &calib_hook.calibration_curve;
 	}
 
-	calib_hook.generate_calibration_curves();
-
-	TreeExportHook	hook(geom, &calib_hook.calibration_curve);
+	TreeExportHook	hook(geom, calibration_curve);
 
 	try
 	{
