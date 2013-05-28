4b1a102557f6d9a4f7e54d5a37aa7d2063eae8dc
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2013-05-28 04:18:54 +0400

    proccessing of right side

--- src/utils/process/ProcessMain.cpp
+++ src/utils/process/ProcessMain.cpp
@@ -18,77 +18,91 @@ ublas::vector<T>    cross( const ublas::vector<T> &a, const ublas::vector<T> &b 
 	return result;
 }
 
-void	Process( TTree *events )
+enum left_right_t {left, right};
+
+double  Psi_L = 1.0447;
+double  Psi_R = -1.0209;
+
+struct track3d_t
 {
-	track_group_t	tg_LX, tg_LY;
+	ublas::vector<double>	a, b;
+};
 
-    events->GetBranch("t3X_track_count")->SetAddress(&tg_LX.track_count);
-    events->GetBranch("t3Y_track_count")->SetAddress(&tg_LY.track_count);
-	int i = -1, j = 0;
-	bool cond;
-	do
-	{
-		i++;
-		events->GetEntry(i);
-		cond = (tg_LX.track_count == 0) || (tg_LY.track_count == 0);
-		if (!cond)
-		{
-			j++;
-		}
-	}
-	while(cond || (j != 2));
+template<left_right_t side>
+track3d_t make_track( int event_id, track_group_t &tg_X, track_group_t &tg_Y )
+{
+	tg_X.c0.resize(tg_X.track_count);
+	tg_X.c1.resize(tg_X.track_count);
+	tg_X.c0_br->SetAddress(tg_X.c0.data());
+	tg_X.c1_br->SetAddress(tg_X.c1.data());
+	tg_X.c0_br->GetEntry(event_id);
+	tg_X.c1_br->GetEntry(event_id);
 
-	tg_LX.c0_br = events->GetBranch("t3X_c0");
-	tg_LX.c1_br = events->GetBranch("t3X_c1");
-	tg_LX.c0.resize(tg_LX.track_count);
-	tg_LX.c1.resize(tg_LX.track_count);
-	tg_LX.c0_br->SetAddress(tg_LX.c0.data());
-	tg_LX.c1_br->SetAddress(tg_LX.c1.data());
-	tg_LX.c0_br->GetEntry(i);
-	tg_LX.c1_br->GetEntry(i);
+	tg_Y.c0.resize(tg_Y.track_count);
+	tg_Y.c1.resize(tg_Y.track_count);
+	tg_Y.c0_br->SetAddress(tg_Y.c0.data());
+	tg_Y.c1_br->SetAddress(tg_Y.c1.data());
+	tg_Y.c0_br->GetEntry(event_id);
+	tg_Y.c1_br->GetEntry(event_id);
 
-	tg_LY.c0_br = events->GetBranch("t3Y_c0");
-	tg_LY.c1_br = events->GetBranch("t3Y_c1");
-	tg_LY.c0.resize(tg_LY.track_count);
-	tg_LY.c1.resize(tg_LY.track_count);
-	tg_LY.c0_br->SetAddress(tg_LY.c0.data());
-	tg_LY.c1_br->SetAddress(tg_LY.c1.data());
-	tg_LY.c0_br->GetEntry(i);
-	tg_LY.c1_br->GetEntry(i);
+	cout << tg_X.track_count << "\t" << tg_Y.track_count << endl;
 
-	cout << tg_LX.track_count << "\t" << tg_LY.track_count << endl;
-
-	double	Psi_L = 1.0447;
-	double	Psi_R = -1.0209;
-	ublas::matrix<double> m1(3, 3), m2(3, 3), A(3, 3);
+	static ublas::matrix<double> m1(3, 3), m2(3, 3), A(3, 3);
 
 	m1(0, 0) = 1; m1(1, 0) = 0;  m1(2, 0) = 0;
 	m1(0, 1) = 0; m1(1, 1) = 0;  m1(2, 1) = 1;
 	m1(0, 2) = 0; m1(1, 2) = -1; m1(2, 2) = 0;
 
-	m2(0, 0) = cos(Psi_L); m2(1, 0) = 0; m2(2, 0) = -sin(Psi_L);
-	m2(0, 1) = 0;          m2(1, 1) = 1; m2(2, 1) = 0;
-	m2(0, 2) = sin(Psi_L); m2(1, 2) = 0; m2(2, 2) = cos(Psi_L);
+	double Psi;
+	if (side == left_right_t::left)
+	{
+		Psi = Psi_L;
+	}
+	else if (side == left_right_t::right)
+	{
+		Psi = Psi_R;
+	}
+	else
+	{
+		throw;
+	}
+
+	m2(0, 0) = cos(Psi); m2(1, 0) = 0; m2(2, 0) = -sin(Psi);
+	m2(0, 1) = 0;        m2(1, 1) = 1; m2(2, 1) = 0;
+	m2(0, 2) = sin(Psi); m2(1, 2) = 0; m2(2, 2) = cos(Psi);
 
 	A = ublas::prod(m1, m2);
 
-	ublas::vector<double> o_L(3), a(3), b(3);
+	ublas::vector<double> o(3), a(3), b(3);
 
-	o_L(0) = 165.2;
-	o_L(1) = 512.4;
-	o_L(2) = 0.3;
+	if (side == left_right_t::left)
+	{
+		o(0) = 165.2;
+		o(1) = 512.4;
+		o(2) = 0.3;
+	}
+	else if (side == left_right_t::right)
+	{
+		o(0) = 159.7;
+		o(1) = -524.0;
+		o(2) = -3.0;
+	}
+	else
+	{
+		throw;
+	}
 
 	b(0) = 1;
-	b(1) = tg_LY.c1[0];
-	b(2) = tg_LX.c1[0];
+	b(1) = tg_Y.c1[0];
+	b(2) = tg_X.c1[0];
 	b = ublas::prod(A, b);
 
 	ublas::unit_vector<double> xk(3, 1);
 	ublas::unit_vector<double> xl(3, 2);
-	a = o_L + ublas::prod(A, xk) * tg_LY.c0[0] + ublas::prod(A, xl) * tg_LX.c0[0];
+	a = o + ublas::prod(A, xk) * tg_Y.c0[0] + ublas::prod(A, xl) * tg_X.c0[0];
 
-	cout << "X\t" << tg_LX.c0[0] << "\t" << tg_LX.c1[0] << endl;
-	cout << "Y\t" << tg_LY.c0[0] << "\t" << tg_LY.c1[0] << endl;
+	cout << "X\t" << tg_X.c0[0] << "\t" << tg_X.c1[0] << endl;
+	cout << "Y\t" << tg_Y.c0[0] << "\t" << tg_Y.c1[0] << endl;
 
 	cout << a << endl;
 	cout << b << endl;
@@ -98,4 +112,47 @@ void	Process( TTree *events )
 	auto	inner_ab = ublas::inner_prod(a, b);
 
 	cout << sqrt(norm_a * norm_a - inner_ab * inner_ab/(norm_b * norm_b)) << endl;
+
+	return track3d_t{a, b};
+}
+
+void	Process( TTree *events )
+{
+	track_group_t	tg_LX, tg_LY, tg_RX, tg_RY;
+
+    events->GetBranch("t3X_track_count")->SetAddress(&tg_LX.track_count);
+    events->GetBranch("t3Y_track_count")->SetAddress(&tg_LY.track_count);
+    events->GetBranch("t4X_track_count")->SetAddress(&tg_RX.track_count);
+    events->GetBranch("t4Y_track_count")->SetAddress(&tg_RY.track_count);
+	int i = -1, j = 0;
+	bool cond;
+	do
+	{
+		i++;
+		events->GetEntry(i);
+		cond = (tg_LX.track_count == 0) || (tg_LY.track_count == 0)
+		    || (tg_RX.track_count == 0) || (tg_RY.track_count == 0);
+		if (!cond)
+		{
+			j++;
+		}
+	}
+	while(cond || (j != 2));
+
+	tg_LX.c0_br = events->GetBranch("t3X_c0");
+	tg_LX.c1_br = events->GetBranch("t3X_c1");
+	tg_LY.c0_br = events->GetBranch("t3Y_c0");
+	tg_LY.c1_br = events->GetBranch("t3Y_c1");
+
+	tg_RX.c0_br = events->GetBranch("t4X_c0");
+	tg_RX.c1_br = events->GetBranch("t4X_c1");
+	tg_RY.c0_br = events->GetBranch("t4Y_c0");
+	tg_RY.c1_br = events->GetBranch("t4Y_c1");
+
+	track3d_t	t_L = make_track<left_right_t::left>(i, tg_LX, tg_LY);
+	track3d_t	t_R = make_track<left_right_t::right>(i, tg_RX, tg_RY);
+
+	auto cr = cross(t_L.b, t_R.b);
+	double cr_norm = norm_2(cr);
+	cout << ublas::inner_prod(cr, t_L.a - t_R.a) / cr_norm << endl;
 }
