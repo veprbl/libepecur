879e88925c4b6bb57d9e02b2337f8f8e8dac18ba
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2013-09-12 16:39:12 +0400

    cmake: don't update git commit id header file if there is no change

--- get_rev_id.sh
+++ get_rev_id.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-echo -n "const char *GIT_COMMIT_ID = \""
+function print_version_definition()
+{
+	echo -n "const char *GIT_COMMIT_ID = \""
 
-git diff --quiet
-UNSTAGED=$?
+	git diff --quiet
+	UNSTAGED=$?
 
-if [ $UNSTAGED = 129 ]; then
-	echo "not in git\";"
-	exit 0
+	if [ $UNSTAGED = 129 ]; then
+		echo "not in git\";"
+		exit 0
+	fi
+
+	git diff --cached --quiet
+	STAGED=$?
+
+	if [ $STAGED = 1 ] || [ $UNSTAGED = 1 ]; then
+		echo "dirty working tree\";"
+		exit 0
+	fi
+
+	git rev-parse HEAD | xargs echo -n
+	echo "\";"
+}
+
+DEFINITION_STR=`print_version_definition`
+FILE_CONTENTS=`cat $1 2> /dev/null`
+if [ "$DEFINITION_STR" != "$FILE_CONTENTS" ]; then
+        echo $DEFINITION_STR > $1
 fi
-
-git diff --cached --quiet
-STAGED=$?
-
-if [ $STAGED = 1 ] || [ $UNSTAGED = 1 ]; then
-	echo "dirty working tree\";"
-	exit 0
-fi
-
-git rev-parse HEAD | xargs echo -n
-echo "\";"
