1a4ba98a5677166f175ab7fadb4cbac9dec68190
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2013-12-09 01:13:01 +0400

    remove old glut include

--- src/utils/process/ProcessVisualizer.hpp
+++ src/utils/process/ProcessVisualizer.hpp
@@ -1,12 +1,6 @@
 #ifndef __PROCESS_VISUALIZER_HPP
 #define __PROCESS_VISUALIZER_HPP
 
-#ifdef __APPLE__
-	#include <GLUT/glut.h>
-#else
-	#include <GL/glut.h>
-#endif
-
 #include "ProcessMain.hpp"
 
 extern process_result_t vis_result;
