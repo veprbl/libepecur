aba3907031b03bcd465adb1d3c50fc2dd24cb762
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2014-07-25 18:40:19 -0400

    travis: update raring repo url

--- .raring-pinning.sh
+++ .raring-pinning.sh
@@ -1,5 +1,5 @@
 echo 'APT::Default-Release "precise";' > /etc/apt/apt.conf.d/01ubuntu
-echo 'deb http://archive.ubuntu.com/ubuntu raring main' >> /etc/apt/sources.list
+echo 'deb http://old-releases.ubuntu.com/ubuntu raring main' >> /etc/apt/sources.list
 echo 'Package: libboost*' >> /etc/apt/preferences
 echo 'Pin: release n=raring' >> /etc/apt/preferences
 echo 'Pin-Priority: 900' >> /etc/apt/preferences
