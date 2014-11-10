841273200dc3883e8a5e034c4ea7f4adb3f793ed
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2014-11-11 01:10:39 +0300

    wip

--- macros/final_plot.C
+++ macros/final_plot.C
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include <set>
+#include <float.h>
 
 #include <TChain.h>
 #include <TH1F.h>
+#include <TMath.h>
 
 TCanvas c1;
 
 void	final_plot()
 {
 	set<int> momentums;
+
+	TProof *p = TProof::Open("");
 
 	FILE *fp = fopen("../contrib/list_apr10",  "r");
 	char run_name[256];
@@ -17,14 +21,16 @@ void	final_plot()
 	{
 		momentums.insert(momentum);
 	}
+double ymin = FLT_MAX, ymax = FLT_MIN;
 int color = 1;
 	for(set<int>::iterator it = momentums.begin(); it != momentums.end(); it++)
 	{
 		int momentum = *it;
-momentum=1060;
+//momentum=1060;
 		fseek(fp, SEEK_SET, 0);
 
 		TChain	c("events");
+		c.SetProof();
 		int run_momentum;
 		while(fscanf(fp, "%s %i", run_name, &run_momentum) != EOF)
 		{
@@ -56,6 +62,15 @@ momentum=1060;
 			, "E1", entries
 			);
 
+		double rval = or->GetBinContent(or->FindBin(run_momentum));
+		double lval = ol->GetBinContent(ol->FindBin(run_momentum));
+		cout << rval << "\t" << lval << endl;
+
+/*		ymin = TMath::Min(TMath::Min(ymin, lval), rval);
+		ymax = TMath::Max(TMath::Max(ymax, lval), rval);*/
+		ymin = 0.0032;
+		ymax = 0.0063;
+
 		TH1F	*rr = new TH1F(Form("r%i_r", momentum), "result_right", (1250-820), 820, 1250);
 		rr->GetXaxis()->SetTitle("p_\\pi, [MeV/c]");
 		rr->GetYaxis()->SetTitle("\\sigma");
@@ -69,10 +84,10 @@ momentum=1060;
 		rl->SetLineColor(color);
 		rl->Sumw2();
 		rl->Divide(ol, n, 1, 100);
-//		delete or;
-//		delete ol;
-//		delete o;
-//		delete n;
+		delete or;
+		delete ol;
+		//delete o;
+		delete n;
 if (color < 9) color++;
 else
 switch(color)
@@ -85,27 +100,29 @@ case 35: color = 38; break;
 case 38: color = 46; break;
 default: color = 1;
 }
-break;
+//break;
 	}
 	fclose(fp);
 
-	TCanvas cr;
+	TCanvas *cr = new TCanvas();
 	for(set<int>::iterator it = momentums.begin(); it != momentums.end(); it++)
 	{
 		int momentum = *it;
 		TH1F *h = (TH1F*)gDirectory->Get(Form("r%i_r", momentum));
 		if (h)
 		{
+			h->GetYaxis()->SetRangeUser(ymin, ymax);
 			h->Draw("same");
 		}
 	}
-	TCanvas cl;
+	TCanvas *cl = new TCanvas();
 	for(set<int>::iterator it = momentums.begin(); it != momentums.end(); it++)
 	{
 		int momentum = *it;
 		TH1F *h = (TH1F*)gDirectory->Get(Form("r%i_l", momentum));
 		if (h)
 		{
+			h->GetYaxis()->SetRangeUser(ymin, ymax);
 			h->Draw("same");
 		}
 	}
