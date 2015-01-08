cf4a9b125a90f6a55f07821a0eacd86ce0d53fe3
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2015-01-08 08:41:31 +0300

    drift_sum_rule.C: allow plotting same correlation in drift time domain

--- macros/drift_sum_rule.C
+++ macros/drift_sum_rule.C
@@ -12,15 +12,39 @@ static TCanvas	c("c", "", 1600.0, 1600.0 / N);
 static TFile	f(gSystem->Getenv("EPECUR_ROOTFILE"), "READ");
 static TTree	*events;
 
-void	mk_plot(int i, double angle, double angle_bin_width)
+void	mk_plot(int i, bool time_domain, double angle, double angle_bin_width)
 {
 	c.cd(i);
 	char *hname = Form("h%i", i);
-	TH2F *h = new TH2F(hname, Form("angle = %.0f \\pm %.0f", angle, angle_bin_width / 2), 100, 0.0, 8.5, 100, 0.0, 8.5);
-	h->SetXTitle("calibration_curve(d3Y2_time), mm");
-	h->SetYTitle("calibration_curve(d3Y1_time), mm");
+	double bin_count, bin_min, bin_max;
+	const char *correlation;
+	if (time_domain)
+	{
+		bin_count = 380;
+		bin_min = 0.0;
+		bin_max = 380;
+		correlation = "d3Y1_time[0]:d3Y2_time[0]";
+	}
+	else
+	{
+		bin_count = 100;
+		bin_min = 0.0;
+		bin_max = 8.5;
+		correlation = "d3Y1_drift_offset[0]:d3Y2_drift_offset[0]";
+	}
+	TH2F *h = new TH2F(hname, Form("angle = %.0f \\pm %.0f", angle, angle_bin_width / 2), bin_count, bin_min, bin_max, bin_count, bin_min, bin_max);
+	if (time_domain)
+	{
+		h->SetXTitle("d3Y2_time, counts");
+		h->SetYTitle("d3Y1_time, counts");
+	}
+	else
+	{
+		h->SetXTitle("calibration_curve(d3Y2_time), mm");
+		h->SetYTitle("calibration_curve(d3Y1_time), mm");
+	}
 	events->Draw(
-		Form("d3Y1_drift_offset[0]:d3Y2_drift_offset[0] >> %s", hname),
+		Form("%s >> %s", correlation, hname),
 		Form(
 			"(d3Y1_num_wires == 1) && (d3Y2_num_wires == 1)"
 			" && (abs(abs(d3Y1_wire_pos[0] - d3Y2_wire_pos[0]) - 1.0) < 0.0000000001)"
@@ -41,7 +65,7 @@ void	drift_sum_rule()
 	const double STEP = 30.0/(N-1);
 	for(int i = 1; i <= N; i++)
 	{
-		mk_plot(i, angle, 0.5 * STEP);
+		mk_plot(i, false, angle, 0.5 * STEP);
 		angle += STEP;
 	}
 }
