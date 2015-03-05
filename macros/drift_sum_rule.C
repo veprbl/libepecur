532ec53c3057b717a11415ca17a4778d73b55f18
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2015-03-06 00:10:23 +0300

    drift_sum_rule: produce plots for all planes of all chambers

--- macros/drift_sum_rule.C
+++ macros/drift_sum_rule.C
@@ -9,7 +9,8 @@
 #include <TTree.h>
 
 static const int N = 5;
-static TCanvas	c("c", "", 1600.0, 1600.0 / N);
+const int NUM_CHAMBERS = 4;
+static TCanvas	c("c", "", 1600.0, 1600.0 * NUM_CHAMBERS / 2 / N);
 static TFile	f(gSystem->Getenv("EPECUR_ROOTFILE"), "READ");
 static TTree	*events;
 
@@ -29,9 +30,11 @@ double	calc_AC(double alpha)
 	return sin((-alpha + 30) * TMath::DegToRad()) * 17.0;
 }
 
-void	mk_plot(int i, bool select_same_plane, bool time_domain, double angle, double angle_bin_width)
+void	mk_plot(int i, const char *chamber, int plane_id, bool select_same_plane, bool time_domain, double angle, double angle_bin_width)
 {
-	char *hname = Form("h%i", i);
+	if ((plane_id != 1) && (plane_id != 3)) throw;
+
+	char *hname = Form("h%s_%i%i_%i", chamber, plane_id, plane_id + 1, i);
 	double bin_count, bin_min, bin_max;
 	const char *var1, *var2, *units, *adjacent_selector;
 	if (time_domain)
@@ -51,38 +54,42 @@ void	mk_plot(int i, bool select_same_plane, bool time_domain, double angle, doub
 	if (time_domain)
 	{
 		units = "counts";
-		var1 = "d3Y1_time[0]";
+		var1 = "d%s%i_time[0]";
 		if (select_same_plane)
 		{
-			var2 = "d3Y1_time[1]";
+			var2 = Form("d%%s%i_time[1]", plane_id);
 		}
 		else
 		{
-			var2 = "d3Y2_time[0]";
+			var2 = Form("d%%s%i_time[0]", plane_id + 1);
 		}
 	}
 	else
 	{
 		units = "mm";
-		var1 = "d3Y1_drift_offset[0]";
+		var1 = "d%s%i_drift_offset[0]";
 		if (select_same_plane)
 		{
-			var2 = "d3Y1_drift_offset[1]";
+			var2 = Form("d%%s%i_drift_offset[1]", plane_id);
 		}
 		else
 		{
-			var2 = "d3Y2_drift_offset[0]";
+			var2 = Form("d%%s%i_drift_offset[0]", plane_id + 1);
 		}
 	}
+	var1 = Form(var1, chamber, plane_id);
+	var2 = Form(var2, chamber);
 	if (select_same_plane)
 	{
-		adjacent_selector = "(d3Y1_num_wires == 2)"
-		" && (abs(abs(d3Y1_wire_pos[0] - d3Y1_wire_pos[1]) - 2.0) < 0.0000000001)";
+		adjacent_selector = "(d%s%i_num_wires == 2)"
+		" && (abs(abs(d%s%i_wire_pos[0] - d%s%i_wire_pos[1]) - 2.0) < 0.0000000001)";
+		adjacent_selector = Form(adjacent_selector, chamber, plane_id, chamber, plane_id, chamber, plane_id);
 	}
 	else
 	{
-		adjacent_selector = "(d3Y1_num_wires == 1) && (d3Y2_num_wires == 1)"
-		" && (abs(abs(d3Y1_wire_pos[0] - d3Y2_wire_pos[0]) - 1.0) < 0.0000000001)";
+		adjacent_selector = "(d%s%i_num_wires == 1) && (d%s%i_num_wires == 1)"
+		" && (abs(abs(d%s%i_wire_pos[0] - d%s%i_wire_pos[0]) - 1.0) < 0.0000000001)";
+		adjacent_selector = Form(adjacent_selector, chamber, plane_id, chamber, plane_id + 1, chamber, plane_id, chamber, plane_id + 1);
 	}
 	h->SetXTitle(Form("%s, %s", var2, units));
 	h->SetYTitle(Form("%s, %s", var1, units));
@@ -90,9 +97,9 @@ void	mk_plot(int i, bool select_same_plane, bool time_domain, double angle, doub
 		Form("%s:%s >> %s", var1, var2, hname),
 		Form(
 			"%s"
-			" && (t3Y_track_count == 1)"
-			" && (abs(abs(atan(t3Y_c1[0])) - %f) < %f)",
-			adjacent_selector, angle * TMath::DegToRad(), angle_bin_width / 2 * TMath::DegToRad()),
+			" && (t%s_track_count == 1)"
+			" && (abs(abs(atan(t%s_c1[0])) - %f) < %f)",
+			adjacent_selector, chamber, chamber, angle * TMath::DegToRad(), angle_bin_width / 2 * TMath::DegToRad()),
 		"COL Z"
 		);
 
@@ -131,14 +138,22 @@ void	drift_sum_rule()
 {
 	events = (TTree*)f.Get("events");
 
-	c.Divide(N);
-
-	double angle = 0.0;
+	const char* chamber[NUM_CHAMBERS] = {"3X", "3Y", "4X", "4Y"};
 	const double STEP = 30.0/(N-1);
-	for(int i = 1; i <= N; i++)
+
+	c.Divide(2 * N, NUM_CHAMBERS);
+	int cell = 1;
+
+	for(int chamber_id = 0; chamber_id < NUM_CHAMBERS; chamber_id++)
 	{
-		c.cd(i);
-		mk_plot(i, false, false, angle, 0.5 * STEP);
-		angle += STEP;
+		for(int plane_id = 1; plane_id <= 3; plane_id += 2)
+		{
+			for(int i = 1; i <= N; i++)
+			{
+				c.cd(cell++);
+				double angle = STEP * (i - 1);
+				mk_plot(i, chamber[chamber_id], plane_id, false, false, angle, 0.5 * STEP);
+			}
+		}
 	}
 }
