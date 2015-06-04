0a9286ec743dab615adb68060d981ef7b7a5dbef
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2015-06-04 23:59:13 +0300

    move kinematics-related stuff to macros/kinematics_common.h

---
+++ macros/kinematics_common.h
@@ -0,0 +1,29 @@
+const double M_p = 938.272046; // MeV
+const double M_pi = 139.57018; // MeV
+
+static double sqr(double x)
+{
+        return x*x;
+}
+
+static double calc_gamma(double beta)
+{
+        return 1/sqrt(1 - sqr(beta));
+}
+
+/**
+ * Calculate beta corresponding to lorentz transformation from laboratory to
+ * the center of inertia system.
+ */
+static double calc_beta(double beam_momentum)
+{
+        return beam_momentum / (M_p + sqrt(sqr(M_pi) + sqr(beam_momentum)));
+}
+
+/**
+ * Calculate momentum in center of inertia system.
+ */
+static double calc_p(double beta)
+{
+        return M_p * beta * calc_gamma(beta);
+}
--- macros/theta_cm_correlation.C
+++ macros/theta_cm_correlation.C
@@ -2,37 +2,9 @@
 
 #include <TCanvas.h>
 
+#include "kinematics_common.h"
+
 TCanvas c1("elastic_kinematics", "", 1200, 600), *c2;
-
-const double M_p = 938.272046; // MeV
-const double M_pi = 139.57018; // MeV
-
-double sqr(double x)
-{
-	return x*x;
-}
-
-double calc_gamma(double beta)
-{
-	return 1/sqrt(1 - sqr(beta));
-}
-
-/**
- * Calculate beta corresponding to lorentz transformation from laboratory to
- * the center of inertia system.
- */
-double calc_beta(double beam_momentum)
-{
-	return beam_momentum / (M_p + sqrt(sqr(M_pi) + sqr(beam_momentum)));
-}
-
-/**
- * Calculate momentum in center of inertia system.
- */
-double calc_p(double beta)
-{
-	return M_p * beta * calc_gamma(beta);
-}
 
 /**
  * Calculate theta_cm from theta_pi in laboratory system
