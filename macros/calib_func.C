16ca4f2381e8c99cc468a7cabc0c24e1a075667e
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2015-06-09 23:07:23 +0300

    calib_func.C: add labels

--- macros/calib_func.C
+++ macros/calib_func.C
@@ -2,13 +2,14 @@ TCanvas c("c", "", 300, 300);
 
 void calib_func()
 {
-	TFile f("~/pass1_full/20041008.root");
-	TTree *events = (TTree*)f.Get("events");
+	TFile *f = new TFile("~/pass1_full/20041008.root");
+	TTree *events = (TTree*)f->Get("events");
 	gStyle->SetPaperSize(5,5);
-	events->Draw("d3Y1_time");
+	new TH1F("h", ";T_{\\text{дрейфа}}, 1;\\frac{dN}{d T_{\\text{дрейфа}}}", 400, 0, 400.);
+	events->Draw("d3Y1_time >> h");
 	c.SaveAs("d3Y1_time.tex");
-	events->Draw("d3Y1_time", "d3Y1_num_wires == 1");
+	events->Draw("d3Y1_time >> h", "d3Y1_num_wires == 1");
 	c.SaveAs("d3Y1_time_1wire.tex");
-	events->Draw("d3Y1_time", "(d3Y1_num_wires == 2) && abs(d3Y1_wire_pos[0] - d3Y1_wire_pos[1]) == 2");
+	events->Draw("d3Y1_time >> h", "(d3Y1_num_wires == 2) && abs(d3Y1_wire_pos[0] - d3Y1_wire_pos[1]) == 2");
 	c.SaveAs("d3Y1_time_adj.tex");
 }
