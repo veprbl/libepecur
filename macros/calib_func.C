421a6ca6445423a040f83defe9b3dcde847c90c9
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2015-06-10 16:17:19 +0300

    calib_func.C: plots for drift time distribution and corresponding calibration function used in production

--- macros/calib_func.C
+++ macros/calib_func.C
@@ -13,4 +13,16 @@ void calib_func()
 	c.SaveAs("d3Y1_time_1wire.tex");
 	events->Draw("d3Y1_time >> h", "(d3Y1_num_wires == 2) && abs(d3Y1_wire_pos[0] - d3Y1_wire_pos[1]) == 2");
 	c.SaveAs("d3Y1_time_adj.tex");
+	events->Draw("d3Y1_time >> h", "(d3Y1_num_wires == 2) && abs(d3Y1_wire_pos[0] - d3Y1_wire_pos[1]) >= 4");
+	c.SaveAs("d3Y1_time_sep.tex");
+	Int_t xfirst  = h->GetXaxis()->GetFirst();
+	Int_t xlast   = h->GetXaxis()->GetLast();
+	for(Int_t bin = xfirst + 1; bin <= xlast; bin++)
+	{
+		h->SetBinContent(bin, h->GetBinContent(bin) + h->GetBinContent(bin - 1));
+	}
+	h->Scale(8.5 / h->GetBinContent(xlast));
+	h->GetYaxis()->SetTitle("d\\text{, мм}");
+	h->GetYaxis()->SetNdivisions();
+	c.SaveAs("d3Y1_calib_sep.tex");
 }
