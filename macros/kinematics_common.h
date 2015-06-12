d579df8f8a583f90558104388d16ed8035f6f032
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2015-06-12 15:58:27 +0300

    move calc_theta_* to kinematics_common.h

--- macros/kinematics_common.h
+++ macros/kinematics_common.h
@@ -37,3 +37,88 @@ static double calc_W(double beta)
 
 	return sqrt(sqr(M_pi) + sqr(p)) + sqrt(sqr(M_p) + sqr(p));
 }
+
+/**
+ * Calculate theta_cm from theta_pi in laboratory system
+ */
+double calc_theta_cm_from_theta_pi_lab(double beam_momentum, double theta_pi_lab)
+{
+	const double beta = calc_beta(beam_momentum);
+	const double gamma = calc_gamma(beta);
+	const double p = calc_p(beta);
+	const double t = tan(theta_pi_lab);
+
+	return
+		acos(
+			-gamma * abs(t) * beta
+			/ p
+			* sqrt(
+				(sqr(M_pi) + sqr(p))
+				/
+				(1 + sqr(gamma * t))
+			)
+		)
+		- atan(1 / (gamma * t));
+}
+
+/**
+ * Calculate theta_cm from theta_p in laboratory system
+ */
+double calc_theta_cm_from_theta_p_lab(double beam_momentum, double theta_p_lab)
+{
+	const double beta = calc_beta(beam_momentum);
+	const double gamma = calc_gamma(beta);
+	const double p = calc_p(beta);
+	const double t = tan(theta_p_lab);
+
+	if (theta_p_lab > M_PI_2)
+	{
+		return NAN;
+	}
+
+	return
+		acos(
+			gamma * abs(t) * beta
+			/ p
+			* sqrt(
+				(sqr(M_p) + sqr(p))
+				/
+				(1 + sqr(gamma * t))
+			)
+		)
+		- atan(1 / (gamma * -t));
+}
+
+/**
+ * Calculate theta_pi in laboratory system from theta_cm
+ */
+double calc_theta_lab_pi(double beam_momentum, double theta_cm)
+{
+	const double beta = calc_beta(beam_momentum);
+	const double gamma = calc_gamma(beta);
+	const double p = calc_p(beta);
+
+	return
+		atan2(
+			p * sin(theta_cm)
+			,
+			gamma * (p * cos(theta_cm) + beta * sqrt(sqr(M_pi) + sqr(p)))
+		);
+}
+
+/**
+ * Calculate theta_p in laboratory ststem from theta_cm
+ */
+double calc_theta_lab_p(double beam_momentum, double theta_cm)
+{
+	const double beta = calc_beta(beam_momentum);
+	const double gamma = calc_gamma(beta);
+	const double p = calc_p(beta);
+
+	return
+		atan2(
+			p * sin(theta_cm)
+			,
+			gamma * (-p * cos(theta_cm) + beta * sqrt(sqr(M_p) + sqr(p)))
+		);
+}
