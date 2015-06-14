b8ec3270c79a12c73775060501f856a3f4cb9edc
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2015-06-15 01:48:41 +0300

    Revert "mk_finals: elastic outcome"
    This reverts commit 446a881ac0a966394102eeae9a58e55612a4577f.

--- macros/mk_finals.py
+++ macros/mk_finals.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python2
 
-for min_angle, max_angle in [(40, 50), (50, 60), (60, 70), (70, 80), (80, 90), (90, 100), (100, 110), (110, 120)]:
+for min_angle, max_angle in [(25, 35), (40, 50), (50, 60), (60, 70)]:
 	print min_angle
 	with open("r.inc", "w") as fp:
 		fp.write(
 	"""\
-	                                   (F2L_x > -200) && (F2L_x < 120) && (LF2_x > -200) && (LF2_x < 120)
-	                                && (F2R_x > -200) && (F2R_x < 120) && (RF2_x > -200) && (RF2_x < 120)
-					&& (fabs(phi_r - phi_l + 3.14) < 0.1)
-					&& (!std::isnan(theta_l))
-					&& (!std::isnan(theta_r))
-					&& (fabs(calc_theta_cm_from_theta_p_lab(beam_momentum, theta_l) - calc_theta_cm_from_theta_pi_lab(beam_momentum, theta_r) + 2 * TMath::DegToRad()) < 3 * TMath::DegToRad())
-	                                && (calc_theta_cm_from_theta_pi_lab(beam_momentum, theta_r) > %i*TMath::DegToRad()) && (calc_theta_cm_from_theta_pi_lab(beam_momentum, theta_r) < %i*TMath::DegToRad())
+	                                   (F2R_x > -200) && (F2R_x < 120) && (RF2_x > -200) && (RF2_x < 120)
+	                                && (theta_r > %i*TMath::DegToRad()) && (theta_r < %i*TMath::DegToRad())
+	                                && std::isnan(theta_l) // Require no track in the left arm\
 	""" % (min_angle, max_angle))
 	
 	with open("l.inc", "w") as fp:
 		fp.write(
 	"""\
 	                                   (F2L_x > -200) && (F2L_x < 120) && (LF2_x > -200) && (LF2_x < 120)
-	                                && (F2R_x > -200) && (F2R_x < 120) && (RF2_x > -200) && (RF2_x < 120)
-					&& (fabs(phi_r - phi_l + 3.14) < 0.1)
-					&& (!std::isnan(theta_l))
-					&& (!std::isnan(theta_r))
-					&& (fabs(calc_theta_cm_from_theta_p_lab(beam_momentum, theta_r) - calc_theta_cm_from_theta_pi_lab(beam_momentum, theta_l) + 2 * TMath::DegToRad()) < 3 * TMath::DegToRad())
-	                                && (calc_theta_cm_from_theta_pi_lab(beam_momentum, theta_l) > %i*TMath::DegToRad()) && (calc_theta_cm_from_theta_pi_lab(beam_momentum, theta_l) < %i*TMath::DegToRad())
+	                                && (theta_l > %i*TMath::DegToRad()) && (theta_l < %i*TMath::DegToRad())
+	                                && std::isnan(theta_r) // Require no track in the right arm\
 	""" % (min_angle, max_angle))
 	
 	with open("finalization.inc", "w") as fp:
@@ -32,7 +24,7 @@ for min_angle, max_angle in [(40, 50), (50, 60), (60, 70), (70, 80), (80, 90), (
 	gStyle->SetPadLeftMargin(0.13-0.9);
 	gStyle->SetPaperSize(3,3);
 	c_combine.cd();
-	c_combine.SaveAs("final_plot_2track_elastic_%i_%i.tex");
+	c_combine.SaveAs("final_plot_1track_%i_%i.tex");
 	exit(0);
 	""" % (min_angle, max_angle))
 	import os
