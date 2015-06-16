a0a3cec55e5201aca40e8ff28b5ea27eb7c0f452
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2015-06-16 15:45:28 +0300

    2track final plot

--- macros/mk_finals.py
+++ macros/mk_finals.py
@@ -6,25 +6,29 @@ for min_angle, max_angle in [(25, 35), (40, 50), (50, 60), (60, 70)]:
 		fp.write(
 	"""\
 	                                   (F2R_x > -200) && (F2R_x < 120) && (RF2_x > -200) && (RF2_x < 120)
+	                                && (F2L_x > -200) && (F2L_x < 120) && (LF2_x > -200) && (LF2_x < 120)
+					&& (abs(phi_r - phi_l + 3.14) > 0.2)
 	                                && (theta_r > %i*TMath::DegToRad()) && (theta_r < %i*TMath::DegToRad())
-	                                && std::isnan(theta_l) // Require no track in the left arm\
+	                                && !std::isnan(theta_l) // Require no track in the left arm\
 	""" % (min_angle, max_angle))
 	
 	with open("l.inc", "w") as fp:
 		fp.write(
 	"""\
-	                                   (F2L_x > -200) && (F2L_x < 120) && (LF2_x > -200) && (LF2_x < 120)
+	                                   (F2R_x > -200) && (F2R_x < 120) && (RF2_x > -200) && (RF2_x < 120)
+	                                && (F2L_x > -200) && (F2L_x < 120) && (LF2_x > -200) && (LF2_x < 120)
+					&& (abs(phi_r - phi_l + 3.14) > 0.2)
 	                                && (theta_l > %i*TMath::DegToRad()) && (theta_l < %i*TMath::DegToRad())
-	                                && std::isnan(theta_r) // Require no track in the right arm\
+	                                && !std::isnan(theta_r) // Require no track in the right arm\
 	""" % (min_angle, max_angle))
 	
 	with open("finalization.inc", "w") as fp:
 		fp.write("""\
 	gStyle->SetPadRightMargin(0.12+0.9);
 	gStyle->SetPadLeftMargin(0.13-0.9);
-	gStyle->SetPaperSize(3,3);
+	gStyle->SetPaperSize(8,8);
 	c_combine.cd();
-	c_combine.SaveAs("final_plot_1track_%i_%i.tex");
+	c_combine.SaveAs("final_plot_2track_%i_%i.tex");
 	exit(0);
 	""" % (min_angle, max_angle))
 	import os
