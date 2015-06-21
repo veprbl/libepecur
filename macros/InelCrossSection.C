68d7afa5c64321a62b10869f583780fb84a1308c
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2015-06-21 15:02:02 +0300

    final_plot: 25 < Theta < 70

--- macros/InelCrossSection.C
+++ macros/InelCrossSection.C
@@ -28,6 +28,7 @@
 #include "InelCrossSection.h"
 #include <TH2.h>
 #include <TStyle.h>
+#include <TMath.h>
 
 #include "kinematics_common.h"
 
@@ -98,7 +99,7 @@ Bool_t InelCrossSection::Process(Long64_t entry)
 		{
 			if (
 				   (F2R_x > -200) && (F2R_x < 120) && (RF2_x > -200) && (RF2_x < 120)
-				&& (theta_r > 0.6) && (theta_r < 0.9)
+				&& (theta_r > 25 * TMath::DegToRad()) && (theta_r < 70 * TMath::DegToRad())
 				&& std::isnan(theta_l) // Require no track in the left arm
 				)
 			{
@@ -110,7 +111,7 @@ Bool_t InelCrossSection::Process(Long64_t entry)
 			}
 			if (
 				   (F2L_x > -200) && (F2L_x < 120) && (LF2_x > -200) && (LF2_x < 120)
-				&& (theta_l > 0.6) && (theta_l < 0.9)
+				&& (theta_l > 25 * TMath::DegToRad()) && (theta_l < 70 * TMath::DegToRad())
 				&& std::isnan(theta_r) // Require no track in the right arm
 				)
 			{
